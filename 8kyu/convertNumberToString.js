fec3b11a5d8f39605d01911694aefc9831a78ccb
Burhan Ali <burhanali.dev@gmail.com>
2022-09-30 23:40:28 -0400

    new code war

---
+++ 8kyu/convertNumberToString.js
@@ -0,0 +1,9 @@
+//www.codewars.com/kata/5265326f5fda8eb1160004c8
+//Convert a Number to a String
+
+function numberToString(num) {
+    let str ='';
+     str = str + num;
+     return str
+     // Return a string of the number here!
+   }

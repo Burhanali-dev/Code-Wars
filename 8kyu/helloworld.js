8636818b7c378fd3bf468bb55350ef0b44a4096e
Burhan Ali <burhanali.dev@gmail.com>
2022-10-04 23:10:02 -0400

    New Challenge

---
+++ 8kyu/helloworld.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/523b4ff7adca849afe000035
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+
+function greet(){
+    return "hello world!"
+  }

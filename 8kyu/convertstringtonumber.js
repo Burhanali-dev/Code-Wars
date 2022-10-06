07073a31f5b5fb1363f6131bd20f29a20e899f46
Burhan Ali <burhanali.dev@gmail.com>
2022-10-05 23:42:39 -0400

    New challenge

---
+++ 8kyu/convertstringtonumber.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/544675c6f971f7399a000e79
+//We need a function that can transform a string into a number.
+
+var stringToNumber = function(str){
+    return parseInt(str);
+  }

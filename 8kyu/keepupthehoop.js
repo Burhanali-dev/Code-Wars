a6319f44a4c116527fe17e8a341f7f474b39b744
Burhan Ali <burhanali.dev@gmail.com>
2022-10-07 01:08:31 -0400

    New challenge

---
+++ 8kyu/keepupthehoop.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+//If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+//If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+function hoopCount (n) {
+    if(n >= 10){
+      return "Great, now move on to tricks"
+    } 
+     else {
+       return "Keep at it until you get it"
+     }
+   }
+
+//** 

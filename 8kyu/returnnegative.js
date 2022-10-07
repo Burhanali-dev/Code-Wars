a121bc332c5e041a277a20e916d4d2f6952a66a9
Burhan Ali <burhanali.dev@gmail.com>
2022-10-06 23:08:50 -0400

    New Challenge

---
+++ 8kyu/returnnegative.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/solutions/javascript
+//In this simple assignment you are given a number and have to make it negative. 
+//But maybe the number is already negative?
+
+function makeNegative(num) {
+    return -Math.abs((num))
+  }
+  

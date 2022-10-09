bf2d5a6df6494ecfd75ea3d26bc2231281a85861
Burhan Ali <burhanali.dev@gmail.com>
2022-10-08 23:54:29 -0400

    New challange

---
+++ 8kyu/calculateavg.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/solutions
+//Write a function which calculates the average of the numbers in a given list.
+function find_average(array) {
+    return array.length>0 ?
+      array.reduce((a,b)=>a+b)/array.length : 0
+  }

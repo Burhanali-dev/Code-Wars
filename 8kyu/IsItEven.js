dd36a4f8dbc91f6b8b516399b5b1e176cfa06fee
Burhan Ali <burhanali.dev@gmail.com>
2022-10-15 15:38:38 -0400

    New challenge

---
+++ 8kyu/IsItEven.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/555a67db74814aa4ee0001b5/javascript
+//Your code will determine if the number passed is even (or not).
+//The function needs to return either a true or false.
+
+function testEven(n) {
+    if (n % 2 === 0){
+      return true
+    }
+  else {
+    return false
+  }
+}

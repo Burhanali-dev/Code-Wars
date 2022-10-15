e7df465d2d535da8c69bb808fb28558adafb94ec
Burhan Ali <burhanali.dev@gmail.com>
2022-10-15 17:13:22 -0400

    New challenge

---
+++ 8kyu/ThirdAngleofaTriangle.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/5a023c426975981341000014
+//You are given two interior angles (in degrees) of a triangle.
+//Write a function to return the 3rd.
+
+function otherAngle(a, b) {
+  
+    return 180 - (a+b)
+  }
+

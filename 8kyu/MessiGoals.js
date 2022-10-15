fc2498e5923c7b8f7fd7c9abf0f77f308aec6e37
Burhan Ali <burhanali.dev@gmail.com>
2022-10-15 03:10:54 -0400

    New challenge

---
+++ 8kyu/MessiGoals.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/55f73be6e12baaa5900000d4/javascript
+//Complete the function to return his total number of goals in all three leagues.
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
+
+//*

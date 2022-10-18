cd8f51c413bd293c68459ba61179a619ec08d55b
Burhan Ali <burhanali.dev@gmail.com>
2022-10-17 23:21:48 -0400

    New challenge

---
+++ 8kyu/lightsabers.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+
+function howManyLightsabersDoYouOwn(name) {
+    if(name==='Zach'){
+      return 18
+    }
+    else {
+      return 0
+    }
+  }
+
+  //** */

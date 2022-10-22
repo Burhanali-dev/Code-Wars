05ac4ff8b16cd0688c866066945ca5023b4dd89b
Burhan Ali <burhanali.dev@gmail.com>
2022-10-22 01:28:44 -0400

    New Challenge

---
+++ 8kyu/grasshopperMessi.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/55ca77fa094a2af31f00002a/javascript
+//Use variables to find the sum of the goals Messi scored in 3 competitions
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+//** */

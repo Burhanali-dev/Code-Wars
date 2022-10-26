2f136a89b83c38e5ac044aa21406a23e63f1d82e
Burhan Ali <burhanali.dev@gmail.com>
2022-10-26 00:33:17 -0400

    New challenge

---
+++ 8kyu/moveFunction.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/javascript
+
+//Create a function for the terminal game that takes the current position 
+//of the hero and the roll (1-6) and return the new position.
+
+function move (position, roll) {
+    return position + (roll*2)
+  }
+
+//** */

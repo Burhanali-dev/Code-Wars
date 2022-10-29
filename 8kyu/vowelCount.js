787ecc622aae3fcc4797df269c343d99df8b29b7
Burhan Ali <burhanali.dev@gmail.com>
2022-10-28 23:56:19 -0400

    New challenge

---
+++ 8kyu/vowelCount.js
@@ -0,0 +1,16 @@
+//https://www.codewars.com/kata/54ff3102c1bad923760001f3/javascript
+//Return the number (count) of vowels in the given string.
+
+//We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+//The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+    let amount = 0
+    for(let i =0; i< str.length; i++){
+      if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'){
+        amount += 1
+      }
+    }
+    return amount
+  }

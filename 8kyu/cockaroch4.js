8beaf287376ae687e3dd8e327cfcf5f7eb4b269c
Burhan Ali <burhanali.dev@gmail.com>
2022-10-31 22:38:51 -0400

    New challenge

---
+++ 8kyu/cockaroch4.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/javascript
+//The cockroach is one of the fastest insects.
+//Write a function which takes its speed in km per hour 
+//and returns it in cm per second, rounded down to the integer (= floored).
+
+//1.08 --> 30
+
+function cockroachSpeed(s) {
+    return Math.floor(s*27.7778)
+  }

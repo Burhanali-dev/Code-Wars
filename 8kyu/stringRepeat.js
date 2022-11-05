6e20a387c7a505b49752d240f898579318e67e4a
Burhan Ali <burhanali.dev@gmail.com>
2022-11-04 20:42:28 -0400

    New challenge

---
+++ 8kyu/stringRepeat.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/javascript
+
+//Write a function that accepts an integer n and a string s as parameters, 
+//and returns a string of s repeated exactly n times.
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }

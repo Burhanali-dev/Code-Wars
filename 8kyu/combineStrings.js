fe1da6e2586f34b8180941a8a7c8e8583148b966
Burhan Ali <burhanali.dev@gmail.com>
2022-11-28 23:56:16 -0500

    New challenge

---
+++ 8kyu/combineStrings.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/55f73f66d160f1f1db000059/javascript
+
+//Create a function named combineNames that accepts two parameters (first and last name). 
+//The function should return the full name.
+
+function combineNames(first,last){
+    return first+' '+last
+  }

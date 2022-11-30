534d6f78cb783e0306a328a9833aeee31f33f05d
Burhan Ali <burhanali.dev@gmail.com>
2022-11-29 23:54:41 -0500

    New challenge

---
+++ 8kyu/chucknorris.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/570669d8cb7293a2d1001473/javascript
+
+//Your task is to be more like Chuck (ha! good luck!). 
+//You must return false without ever actually using the word false...
+
+function ifChuckSaysSo(){
+    return !true 
+    }

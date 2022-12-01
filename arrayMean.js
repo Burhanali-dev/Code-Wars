f4477f2f3e0a40de90bedd90461ba64974a60c17
Burhan Ali <burhanali.dev@gmail.com>
2022-11-30 23:54:04 -0500

    New challenge

---
+++ arrayMean.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/55d277882e139d0b6000005d/javascript
+
+//Find the mean (average) of a list of numbers in an array.
+
+var findAverage = function (nums) {
+    return nums.reduce((a,b)=>a+b,0)/nums.length
+  }
